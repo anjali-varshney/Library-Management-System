deecc6fbec64d515eca01fa4c2b7fb08dc3f5e08
anjali-varshney <anjalivarshney019@gmail.com>
2025-03-27 19:08:45 +0530

    changed book repository

--- src/main/java/com/ajackus/Library_Management_System/repository/BookRepository.java
+++ src/main/java/com/ajackus/Library_Management_System/repository/BookRepository.java
@@ -15,4 +15,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
      */
 
     Optional<Book> findByTitle(String title);
+    boolean existsByTitle(String title);
+    Optional<Book> findByIsbn(String isbn);
+
 }
