fef9b379a82a4daefd73738724a960c7fe977635
anjali-varshney <anjalivarshney019@gmail.com>
2025-03-27 19:06:38 +0530

    updated book model

--- src/main/java/com/ajackus/Library_Management_System/model/Book.java
+++ src/main/java/com/ajackus/Library_Management_System/model/Book.java
@@ -1,12 +1,19 @@
 package com.ajackus.Library_Management_System.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
 @Data
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
+
     private  String title;
     private  String author;
     private  String genre;
     private  String availabilityStatus;
+    private String isbn;
 }
