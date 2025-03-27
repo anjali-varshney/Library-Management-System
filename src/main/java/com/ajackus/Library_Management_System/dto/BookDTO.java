9f973f14fdf7809bb24235ffdbd6d0ae28793f3b
anjali-varshney <anjalivarshney019@gmail.com>
2025-03-27 19:04:56 +0530

    added book dto

--- src/main/java/com/ajackus/Library_Management_System/dto/BookDTO.java
+++ src/main/java/com/ajackus/Library_Management_System/dto/BookDTO.java
@@ -1,21 +1,35 @@
 package com.ajackus.Library_Management_System.dto;
 
+import com.ajackus.Library_Management_System.model.Book;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private  Long id;
 
     private  String title;
     private  String author;
     private  String genre;
     private  String availabilityStatus;
+    private String isbn;
+
+    public BookDTO(Book book) {
+        this.id = book.getId();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        this.genre = book.getGenre();
+        this.availabilityStatus = book.getAvailabilityStatus();
+        this.isbn = book.getIsbn();
+    }
+
 }
